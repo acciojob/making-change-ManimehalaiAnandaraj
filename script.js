f55c4390164b38df36e86ccfb8a910c51084493a
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-18 15:26:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const makeChange = (c) => {
   // your name here
+	const coins={
+		q:25,
+		d:10,
+		n:5,
+		p:1
+		
+	
+	};
+	
+const result={
+		q:0,
+		d:0,
+		n:0,
+		p:0
 };
+		for(let i=0;i<coins.length;i++)
+			{
+				while (c>=coins[i]) {
+					c -=coins[i];
+					result[i]++;
+					
+					
+				}
+			}
+
+	
+return result;
+	
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
